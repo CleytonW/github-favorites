a84a4f368ada53dbee9c3d01c1c5ba9a5750b878
Cleyton Melo <cleytonw11@gmail.com>
2023-01-10 23:54:29 -0300

    adding functions that connecting the application with github

--- favorites.js
+++ favorites.js
@@ -1,3 +1,18 @@
+export class GithubUser {
+  static search(username) {
+    const endpoint = `https://api.github.com/users/${username}`
+
+    return fetch(endpoint)
+    .then(data => data.json())
+    .then(({ login, name, public_repos, followers }) => ({
+      login,
+      name,
+      public_repos,
+      followers
+    }))
+  }
+}
+
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
@@ -5,29 +20,21 @@ export class Favorites {
   }
 
   load() {
-    this.entries = [
-      {
-        login: 'CleytonW',
-        name: 'Cleyton Melo',
-        public_repos: '167',
-        followers: '345'
-      },
-      {
-        login: 'maykbrito',
-        name: 'Mayk Brito',
-        public_repos: '300',
-        followers: '120000'
-      }
-    ]
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  async add(username) {
+    const user = await GithubUser.search(username)
   }
 
   delete(user) {
-    const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+    const filteredEntries = this.entries
+    .filter(entry => entry.login !== user.login)
 
-    console.log(filteredEntries)
+    this.entries = filteredEntries
+    this.update()
   }
 }
-
 
 export class FavoritesView extends Favorites {
   constructor(root){
@@ -36,6 +43,16 @@ export class FavoritesView extends Favorites {
     this.tbody = this.root.querySelector('table tbody')
 
     this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
   }
 
   update(){
